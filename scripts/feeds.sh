b267e02b122f05de39559f2512a9a17c4dc27fc6
oppen321 <zj18139624826@gmail.com>
2024-10-25 20:02:54 +0800

    Update and rename feeds to feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #=================================================
-# DaoDao's script
+# Zero's script
 #=================================================
 ##添加自己的插件库
 echo -e "\nsrc-git Zero https://github.com/oppen321/Zero-IPK" >> feeds.conf.default
