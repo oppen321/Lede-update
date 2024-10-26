f75c04b49188acf63fabc23e911eb578bf2b7c43
oppen321 <zj18139624826@gmail.com>
2024-10-26 08:34:51 +0800

    更新 diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-## Zero源
-echo -e "\nsrc-git Zero https://github.com/oppen321/Zero-IPK" >> feeds.conf.default
-
-./scripts/feeds update -a
-./scripts/feeds install -a
-
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
@@ -29,6 +23,11 @@ rm -rf feeds/packages/lang/golang
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
+## Zero源
+echo -e "\nsrc-git Zero https://github.com/oppen321/Zero-IPK" >> feeds.conf.default
+
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # golong1.23依赖
 #git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
