541260c3b6b8a518895ceac8ce1c66b97d6980ab
oppen321 <zj18139624826@gmail.com>
2024-12-01 11:46:27 +0800

    Update Change source address.sh

--- scripts/Change source address.sh
+++ scripts/Change source address.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
- mkdir -p files/etc/opkg
+mkdir -p files/etc/opkg
 
- 
+# 设置新的源地址
+NEW_SOURCES="
+src/gz openwrt_base https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/base
+src/gz openwrt_luci https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/luci
+src/gz openwrt_packages https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/packages
+src/gz openwrt_routing https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/routing
+src/gz openwrt_telephony https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/telephony
+"
+
+# 创建 opkg 配置文件并写入源地址
+echo "$NEW_SOURCES" > files/etc/opkg.conf
+
